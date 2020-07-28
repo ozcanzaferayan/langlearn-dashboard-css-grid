f252ea333b5041110169fedfdd019ac9c7b97a25
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-07-28 04:04:22 +0300

    Add navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 // JS
+function formatTitleDate(date = new Date()) {
+    let { day, month, year, weekday } = new Intl.DateTimeFormat('en', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        weekday: 'long',
+    }).formatToParts(date).reduce((acc, part) => {
+        if (part.type != 'literal') {
+            acc[part.type] = part.value;
+        }
+        return acc;
+    }, Object.create(null));
+    return `${day} ${month} ${year}, ${weekday}`;
+}
+
+document.getElementsByClassName('date')[0].textContent = formatTitleDate();
